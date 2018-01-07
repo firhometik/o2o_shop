1e6adc2453df2c0668cf46498f6d71601173d4c7
lvxudong <lvxudong@abc.com>
2018-01-07 23:10:37 +0800

    第二次提交

--- application/build.php
+++ application/build.php
@@ -14,12 +14,24 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
-    ],
+    // 'demo'     => [
+    //     '__file__'   => ['common.php'],
+    //     '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+    //     'controller' => ['Index', 'Test', 'UserType'],
+    //     'model'      => ['User', 'UserType'],
+    //     'view'       => ['index/index'],
+    // ],
     // 其他更多的模块定义
+     'index'     => [
+        '__dir__'    => ['common', 'controller', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'User'],
+        'model'      => ['User', ],
+        'view'       => ['index/index','user/index'],
+    ],
+    'admin'     => [
+        '__dir__'    => ['common', 'controller', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'User'],
+        'model'      => ['User', ],
+        'view'       => ['index/index','user/index'],
+    ],
 ];
